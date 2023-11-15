a9be4f7a14ed2ac13061eab83198a03cc6723f57
ZekeLionhart <67716185+ZekeLionhart@users.noreply.github.com>
2023-11-15 17:11:54 -0300

    improve velocity calculation of trebuchet's attack

--- Assets/Scripts/Enemies/TrebuchetManager.cs
+++ Assets/Scripts/Enemies/TrebuchetManager.cs
@@ -12,8 +12,9 @@ public class TrebuchetManager : EnemyBase
     protected override void Attack()
     {
         Rigidbody2D shotRigid = Instantiate(ammo, ammoSpawn.position, ammoSpawn.rotation);
-        Vector3 force = 1.73f * (new Vector3(4f, 4f, 0f) - transform.position);
-        shotRigid.velocity = force;
+        //Vector3 force = 1.73f * (new Vector3(4f, 4f, 0f) - transform.position);
+        //shotRigid.velocity = force;
+        shotRigid.velocity = CalculateBallisticVelocity(new Vector2(-6.5f, -0.5f));
 
         OnBoulderInstantiated(shotRigid, damage);
     }
@@ -22,5 +23,15 @@ public class TrebuchetManager : EnemyBase
     {
         if (onReloadSfx != null)
             onReloadSfx.Play();
+    }
+
+    private Vector3 CalculateBallisticVelocity(Vector3 targetPosition)
+    {
+        Vector3 dir = targetPosition - transform.position; // get target direction
+        dir.y = 0f;  // retain only the horizontal direction
+        float dist = dir.magnitude;  // get horizontal distance
+        dir.y = dist;  // set elevation to 45 degrees
+        float vel = Mathf.Sqrt(dist * Physics.gravity.magnitude);
+	    return vel * dir.normalized;  // returns Vector3 velocity
     }
 }
