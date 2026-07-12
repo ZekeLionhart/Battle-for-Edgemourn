e18a89c443d3075ca7bca7db9421a4345396cede
ZekeLionhart <67716185+ZekeLionhart@users.noreply.github.com>
2026-07-12 02:38:28 -0300

    adjust burn mark to compensate for height

--- Assets/Scripts/Powers/Projectiles/ExplosionManager.cs
+++ Assets/Scripts/Powers/Projectiles/ExplosionManager.cs
@@ -2,10 +2,38 @@ using UnityEngine;
 
 public class ExplosionManager : ProjectileManager
 {
+    [SerializeField] private Transform burnMark;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private CircleCollider2D explosionCollider;
+    private readonly float rayDistance = 10f;
+
+    private void Awake()
+    {
+        PlaceBurnMark();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(TagNames.Enemy))
             OnEnemyHit(gameObject, collision.gameObject, powerType, damageType, damage);
+    }
+
+    private void PlaceBurnMark()
+    {
+        Vector2 origin = burnMark.transform.position + Vector3.up; //gives some distance to avoid clipping
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, rayDistance, groundLayer);
+
+        if (hit)
+        {
+            //Position
+            burnMark.position = hit.point;
+
+            //Scale
+            float distance = hit.distance - explosionCollider.radius - 1f; //1f compensates for Vector3.up
+            float scale = Mathf.Lerp(1f, 0f, distance / (rayDistance - explosionCollider.radius - 8f));
+            burnMark.transform.localScale = Vector3.one * scale;
+        }
     }
 
     private void DestroySelf()
