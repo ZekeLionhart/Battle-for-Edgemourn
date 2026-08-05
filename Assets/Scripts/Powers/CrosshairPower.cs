8a7fe8f84eb8bc384e0f9eaaef61c659ef79e49e
ZekeLionhart <67716185+ZekeLionhart@users.noreply.github.com>
2026-08-05 16:45:08 -0300

    fix gauge powers movable outside clickable area

--- Assets/Scripts/Powers/CrosshairPower.cs
+++ Assets/Scripts/Powers/CrosshairPower.cs
@@ -47,7 +47,7 @@ public class CrosshairPower : PowerController
 
     protected virtual void HandleAimMovement()
     {
-        if ((aimStyleManual && !Input.GetButton(KeyNames.Fire)) || !offCooldown)
+        if ((aimStyleManual && !Input.GetButton(KeyNames.Fire)) || !offCooldown || !isMouseInside)
             return;
 
         aimingPoint.position += aimSpeed * directionMult * Time.deltaTime * Vector3.right;
@@ -59,7 +59,7 @@ public class CrosshairPower : PowerController
 
     protected virtual void HandleAimRotation()
     {
-        if ((aimStyleManual && !Input.GetButton(KeyNames.Fire)) || !offCooldown)
+        if ((aimStyleManual && !Input.GetButton(KeyNames.Fire)) || !offCooldown || !isMouseInside)
             return;
 
         aimingPoint.Rotate(0, 0, directionMult * aimSpeed * Time.deltaTime);
