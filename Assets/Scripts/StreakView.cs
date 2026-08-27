b56c3babb6e1dbc35a85405290909b2d7ad83a1b
ZekeLionhart <67716185+ZekeLionhart@users.noreply.github.com>
2026-08-27 16:33:02 -0300

    improve the streak wheel's art

--- Assets/Scripts/StreakView.cs
+++ Assets/Scripts/StreakView.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 public class StreakView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI streakLabel;
+    [SerializeField] private Image background;
     [SerializeField] private Image streakWheel;
     [SerializeField] private Animator animator;
+    [SerializeField] private Color fullColor;
+    [SerializeField] private Color halfColor;
+    [SerializeField] private Color emptyColor;
 
     private void Awake()
     {
@@ -26,17 +30,35 @@ public class StreakView : MonoBehaviour
 
     private void Update()
     {
-        streakWheel.fillAmount = StreakManager.Instance.Timer / StreakManager.Instance.StreakWindow;
+        UpdateColor(streakWheel.fillAmount = StreakManager.Instance.Timer / StreakManager.Instance.StreakWindow);
+    }
+
+    private void UpdateColor(float fill)
+    {
+        if (fill > 0.5f)
+        {
+            // Green -> Yellow
+            float t = (fill - 0.5f) / 0.5f;
+            streakWheel.color = Color.Lerp(halfColor, fullColor, t);
+        }
+        else
+        {
+            // Yellow -> Red
+            float t = fill / 0.5f;
+            streakWheel.color = Color.Lerp(emptyColor, halfColor, t);
+        }
     }
 
     private void UpdateStreak(int newStreak)
     {
         streakLabel.text = "x" + newStreak;
+        background.gameObject.SetActive(true);
         animator.SetTrigger(ParameterNames.Increase);
     }
 
     private void EndStreak()
     {
         streakLabel.text = "";
+        background.gameObject.SetActive(false);
     }
 }
